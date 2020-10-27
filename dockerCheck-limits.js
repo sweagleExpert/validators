6a4f633570854f9d2d6605602a8bea66d40c7795
dimitris-sweagle <dimitris@sweagle.com>
2020-10-27 16:56:07 +0100

    make it more generic

--- dockerCheck-limits.js
+++ dockerCheck-limits.js
@@ -1,59 +1,103 @@
-// description: Check if docker limits (cpus and memory) have been defined for each service
+// description: Check replicaCount if superior to 1, needed for containers in production that must be highly available
+// description: This is based on generic simple validator rule with a static keyAndWantedValues
 
-// dockerCheck-limits.js
-// Creator:   Dimitris
-// Version:   1.0 - Add exception list
+// generic-simple-validator.js
+// Creator:   Dimitris for customer POC
+// Version:   1.0 - First
 //
-var servicesNodeName = "services";
-var paramToSearch = ["deploy.resources.limits.cpus", "deploy.resources.limits.memory"];
+var subsetToCheck = cds[0];
+var pathAndWantedValues = {
+  "*/resources/limits/cpu" : ["<", "500m"],
+  "*/resources/limits/memory" : ["<", "1Gi"]
+};
+var subsetToCheck = cds[0];
+var operatorArray = ["=", ">", "<", "IN", "NOT IN"];
+var pathSeparator = '/';
+
+// Defines if error must include full path of key found
+var includePath = true;
+// Defines the max number of errors to return
+var maxErrorDisplay = 5;
 var errorFound = false;
 var errors = [];
 var description = '';
 
-// Get list of exceptions
-var svcExceptionList = getSubsetByName(metadataset, "services-exceptions");
-
-// Get list of services
-var servicesSubset = getSubsetByName(metadataset, servicesNodeName);
-if (servicesSubset == "ERROR: NOT FOUND") {
-    errorFound = true;
-    errors.push("*** No service defined !");
-} else {
-    for (var item in servicesSubset) {
-        //console.log("service="+item);
-        if (! svcExceptionList.hasOwnProperty(item)) {
-            for (var i=0; i<paramToSearch.length; i++) {
-                //console.log("param="+paramToSearch[i]);
-                var result = getValueByPath(servicesSubset[item], paramToSearch[i], ".");
-                if (result == "ERROR: NOT FOUND") {
-                    errorFound = true;
-        			errors.push("*** For service ("+item+"): Limits ("+paramToSearch[i]+") not Found");
-                }
-            }
-        }
-    }
-
+for (var path in pathAndWantedValues) {
+  if (errors.length >= maxErrorDisplay) { break; }
+  var operator = pathAndWantedValues[path][0];
+  var value = pathAndWantedValues[path][1];
+  //console.log("operator="+operator);
+  //console.log("value="+value);
+  if (operatorArray.includes(operator)) {
+    checkPathValue(subsetToCheck, path, operator, value, [], 0, pathSeparator);
+  } else {
+    errorFound=true;
+    errors.push("## operator ("+operator+") unknown");
+  }
 }
 
-description = errors.join(', ');
+if (errorFound) {
+  if (errors.length < maxErrorDisplay) { description = "ERRORS: " + errors.join(' '); }
+  else { description = "ERRORS: only first "+maxErrorDisplay+" errors are displayed:" + errors.join(' '); }
+} else { description = "Validation passed successfully"; }
+
 return {description: description, result:!errorFound};
 
 
-// Return the value of a specific key based on its name
-// If the key is a node, then it returns a subset
-// If multiple keys with same name, the first value found is returned
-// If not found, then "ERROR: NOT FOUND" is returned
-function getSubsetByName(mds, name) {
-  var value = "ERROR: NOT FOUND";
-  for (var item in mds) {
-    // check if the key equals to the search term
-    if (item === name ) {
-      return mds[item];
+// Check the value of a specific key based on its name
+function checkPathValue(subset, path, operator, refValue, prefix, level, pathSeparator) {
+  var pathSteps =  path.split(pathSeparator);
+
+  if (pathSteps[0] == "*") {
+    // This is dynamic path, search for the root node
+    subset = getValueByName(subset, pathSteps[1], prefix, level, pathSeparator);
+    if (subset == "ERROR: NOT FOUND") {
+      errorFound=true;
+      errors.push("## "+path+" node "+pathSteps[1]+" not found");
+      return false;
     }
+    // Remove first 2 items of the array and continue as if static path now
+    level = level+1;
+    prefix[level] = "*";
+    level = level+1;
+    prefix[level] = pathSteps[1];
+    pathSteps.shift(); pathSteps.shift();
+  }
+  for (var i = 0; i < pathSteps.length; i++ ) {
+    if (subset.hasOwnProperty(pathSteps[i])) {
+      level = level+1;
+      prefix[level] = pathSteps[i];
+      subset = subset[pathSteps[i]];
+    } else {
+      errorFound=true;
+      errors.push("## "+path+" node "+pathSteps[i]+" not found");
+      return false;
+    }
+  }
+  var item=pathSteps[pathSteps.length-1];
+  //console.log(subset);
+  // check if the key equals to the search term
+  var result = checkValue(subset, operator, refValue);
+  if (! result) {
+    errorFound = true;
+    var pre=prefix[0];
+    for (var i=1; i<level;i++) { pre = pre + pathSeparator + prefix[i]; }
+    if (includePath) { errors.push("## Key ("+pre+pathSeparator+item+") has value ("+subset+") not respecting rule ("+operator+" "+refValue+") or not in good format"); }
+    else { errors.push("## Key ("+item+") has value ("+subset+") not respecting rule ("+operator+" "+refValue+") or not in good format"); }
+  }
+}
+
+
+function getValueByName(subset, name, prefix, level, pathSeparator) {
+  var value = "ERROR: NOT FOUND";
+  for (var item in subset) {
     // check if the key points to an object
-    if  (typeof (mds[item]) === "object") {
+    if  (typeof (subset[item]) === "object") {
+      // check if the key equals to the search term
+      if (item === name ) { return subset[item]; }
       // if value is an object call recursively the function to search this subset of the object
-      value = getSubsetByName(mds[item], name);
+      prefix[level] = item;
+      value = getValueByName(subset[item], name, prefix, level+1, pathSeparator);
       // if key was found, returns it
       if (value != "ERROR: NOT FOUND") { return value; }
       // if not, continue the loop
@@ -62,21 +106,44 @@ function getSubsetByName(mds, name) {
   return value;
 }
 
-// Return the value of a specific key based on its complete path
-// If the key is a node, then it returns "ERROR: NOT FOUND"
-// If not found, then "ERROR: NOT FOUND" is returned
-function getValueByPath(mds, path, pathSeparator) {
-  var pathSteps =  path.split(pathSeparator);
-  var subset = mds;
-  for (var i = 0; i < pathSteps.length; i++ ) {
-    if (subset.hasOwnProperty(pathSteps[i])) {
-      subset = subset[pathSteps[i]];
-    } else {
-      return "ERROR: NOT FOUND";
-    }
+
+function checkValue (val, op, refVal) {
+  //console.log("op"+op);
+  //console.log("val"+val);
+  //console.log("refVal"+refVal);
+  switch (op) {
+    case ">" :
+      if (isNaN(val)) { val = convertToNum(val); }
+      if (isNaN(refVal)) { refVal = convertToNum(refVal); }
+      if (val <= refVal) { return false; }
+      break;
+    case "<" :
+      if (isNaN(val)) { val = convertToNum(val); }
+      if (isNaN(refVal)) { refVal = convertToNum(refVal); }
+      if (val >= refVal) { return false; }
+      break;
+    case "=" :
+      if (val != refVal) { return false; }
+      break;
+    case "IN" :
+      var valArray = refVal.split(',');
+      // console.log(valArray);
+      if (!(valArray.includes(val))) { return false; }
+      break;
+    case "NOT IN" :
+      var valArray = refVal.split(',');
+      if (valArray.includes(val)) { return false; }
+      break;
   }
-  if (typeof (subset) === "object") {
-      return "ERROR: NOT FOUND";
-  }
-  return subset;
+  return true;
+}
+
+function convertToNum (val) {
+  // This is RAM in megabytes, return as is without unit
+  if (val.toLowerCase().endsWith("mi")) { return Number.parseFloat(val.slice(0, -2)); }
+  // This is RAM in Gigabytes, multiply it by 1024 to compare with megabytes
+  if (val.toLowerCase().endsWith("gi")) { return Number.parseFloat(val.slice(0, -2))*1024; }
+  // This is CPU milliseconds, divide it to 1000 to compare it with CPU units
+  if (val.toLowerCase().endsWith("m")) { return Number.parseFloat(val.slice(0, -1))/1000; }
+  return false;
 }
